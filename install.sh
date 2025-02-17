e3d394894ed0131b2b7f3614e9f5b8c326af66f8
Dz <99237626+Dafatrizaaa@users.noreply.github.com>
2025-02-17 18:14:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,101 +1,35 @@
 #!/bin/bash
-echo "SCRIPT AUTO INSTALL WINDOWS by HIDESSH"
-echo
-echo "Pilih OS yang ingin anda install"
-echo "[1] Windows 2019(Default)"
-echo "[2] Windows 2016"
-echo "[3] Windows 2012"
-echo "[4) Windows 10"
-echo "[5] Chat Ryan Untuk Add OS lain"
+set -e
 
-read -p "Pilih [1]: " PILIH OS
+echo "=== AUTO INSTALL WINDOWS RDP ==="
 
-case "$PILIHOS" in
-	1|"") PILIHOS="http://143.198.203.169/Windows10.gz";;
-	2) PILIHOS="https://file.nixpoin.com/windows2016.gz";;
-	3) PILIHOS="https://download1589.mediafire.com/om29odxbrj5g38gUMD7RWK7ZL1IsI9J8Z5o2Ql9VbYIwq_zdf6YFgHJC6NCcQeWRIgW0YtHu3NhXPbBzbcYeOtKMHW2MmujTqFdnoV95L0rwtw0BKdv-PWJhhor4Wxu8K7CiQIKJEwobcL8REtIskfXJW6PjUjJYSQi1XCyxiGsQWw/2fclsa87a89ro29/ws12%2853058%29.gz";;
-	4) PILIHOS="http://143.198.203.169/Windows10.gz";;
-	5) read -p "[?] Masukkan Link GZ mu : " PILIHOS;;
-	*) echo "[!] Pilihan salah"; exit;;
-esac
+# Pilih OS secara otomatis (default: Windows 2019)
+OS_URL="http://143.198.203.169/Windows10.gz"
+PASSWORD="SecureRDP2025!"
+PORT=3389
 
-echo "[*] Password yang saya buat sudah masuk wordlist bruteforce, silahkan masukkan password yang lebih aman!"
-read -p "[?] Masukkan password untuk akun Administrator Rdp anda(minimal 12 karakter) : " PASSADMIN
+# Unduh dan ekstrak image Windows
+echo "[*] Mengunduh Windows Image..."
+wget -O windows.gz "$OS_URL"
+echo "[*] Ekstrak image..."
+gunzip -c windows.gz > /dev/sda
 
-IP4=$(curl -4 -s icanhazip.com)
-GW=$(ip route | awk '/default/ { print $3 }')
+# Konfigurasi password Administrator
+echo "[*] Mengatur password Administrator..."
+echo -e "$PASSWORD\n$PASSWORD" | passwd Administrator
 
+# Konfigurasi firewall dan port
+echo "[*] Mengatur firewall dan membuka port RDP..."
+ufw allow $PORT/tcp
+ufw enable
 
-cat >/tmp/net.bat<<EOF
-@ECHO OFF
-cd.>%windir%\GetAdmin
-if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
-echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
-"%temp%\Admin.vbs"
-del /f /q "%temp%\Admin.vbs"
-exit /b 2)
-net user Administrator $PASSADMIN
+# Aktifkan layanan RDP
+echo "[*] Mengaktifkan layanan RDP..."
+systemctl enable xrdp
+echo "[*] Memulai ulang layanan RDP..."
+systemctl restart xrdp
 
-
-for /f "tokens=3*" %%i in ('netsh interface show interface ^|findstr /I /R "Local.* Ethernet Ins*"') do (set InterfaceName=%%j)
-netsh -c interface ip set address name="Ethernet Instance 0" source=static address=$IP4 mask=255.255.240.0 gateway=$GW
-netsh -c interface ip add dnsservers name="Ethernet Instance 0" address=8.8.8.8 index=1 validate=no
-netsh -c interface ip add dnsservers name="Ethernet Instance 0" address=8.8.4.4 index=2 validate=no
-
-cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
-del /f /q net.bat
-exit
-EOF
-
-
-cat >/tmp/dpart.bat<<EOF
-@ECHO OFF
-echo WaRNiNG HaZaRD 
-echo JENDELA INI JANGAN DITUTUP
-echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 6969, UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:6969
-echo KETIK YES LALU ENTER!
-
-cd.>%windir%\GetAdmin
-if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
-echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
-"%temp%\Admin.vbs"
-del /f /q "%temp%\Admin.vbs"
-exit /b 2)
-
-set PORT=6969
-set RULE_NAME="Open Port %PORT%"
-
-netsh advfirewall firewall show rule name=%RULE_NAME% >nul
-if not ERRORLEVEL 1 (
-    rem Rule %RULE_NAME% already exists.
-    echo Hey, you already got a out rule by that name, you cannot put another one in!
-) else (
-    echo Rule %RULE_NAME% does not exist. Creating...
-    netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=TCP localport=%PORT%
-)
-
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 6969
-
-ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
-ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
-START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
-
-del /f /q "%SystemDrive%\diskpart.extend"
-cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
-del /f /q dpart.bat
-timeout 50 >nul
-del /f /q ChromeSetup.exe
-echo JENDELA INI JANGAN DITUTUP
-exit
-EOF
-
-wget --no-check-certificate -O- $PILIHOS | gunzip | dd of=/dev/vda bs=3M status=progress
-
-mount.ntfs-3g /dev/vda2 /mnt
-cd "/mnt/ProgramData/Microsoft/Windows/Start Menu/Programs/"
-cd Start* || cd start*; \
-wget https://nixpoin.com/ChromeSetup.exe
-cp -f /tmp/net.bat net.bat
-cp -f /tmp/dpart.bat dpart.bat
-
-echo "reboot Rdp dulu mazzeh baru bisa pake"
+echo "=== Instalasi Selesai! ==="
+echo "Gunakan RDP dengan IP VPS dan port $PORT"
+echo "Username: Administrator"
+echo "Password: $PASSWORD"
